013eb46f5cab61097e25872ec42cd64cd42d3c93
isadorahoeckele <isadora.hoeckele@escola.pr.gov.br>
2026-06-08 08:32:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function abrirQuiz() {
+    alert("Quiz sobre sustentabilidade em breve!");
+}
 
+function abrirCalculadora() {
+    alert("Calculadora de impacto ambiental em desenvolvimento!");
+}
